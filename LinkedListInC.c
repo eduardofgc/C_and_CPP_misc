dd5f229701d95a2041661ce1ec8c080bfa88478e
Eduardo Rocha <dudurbiagini@gmail.com>
2025-04-04 01:35:42 -0300

    remade LinkedList in C

---
+++ LinkedListInC.c
@@ -0,0 +1,49 @@
+//i didnt know you could do linked lists without oop. i guess methods and constructors really are totally optional
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int val;
+    Node *prox;
+} Node;
+
+int main(){
+    Node* novo;
+    Node* head;
+    Node* next;
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        novo = malloc(sizeof(Node));
+        novo->val = i;
+
+        if (i == 0){
+            head = novo;
+            next = novo;
+
+            printf("%d", novo->val);
+        }
+        else{
+            next->prox = novo;
+            next = novo;
+
+            printf("%d", novo->val);
+        }
+    }
+
+    next = head;
+
+    while (next != NULL){
+        Node* temp = next->prox;
+        free(next);
+
+        next = temp;
+    }
+
+
+
+    return 0;
+}
