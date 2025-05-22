ca23afcf598052d29eaf4215391d073e8237b2b0
Eduardo Rocha <dudurbiagini@gmail.com>
2025-05-22 15:26:32 -0300

    added swap function to Stack.c

--- Stuff/Stack.c
+++ Stuff/Stack.c
@@ -3,8 +3,10 @@
 #include <stdbool.h>
 
 typedef struct Node{
-    int valor;
+    int idade;
     struct Node* prox;
+    struct Node* past;
+    bool priority1, priority2;
 } Node;
 
 typedef struct List{
@@ -25,7 +27,7 @@ List* queue(){
 
 void enqueue(List* myList, int index){
     Node* newNode = (Node*) malloc(sizeof(Node));
-    newNode->valor = index;
+    newNode->idade = index;
 
     if (myList->last == NULL){
         myList->head = newNode;
@@ -33,6 +35,7 @@ void enqueue(List* myList, int index){
     }
     else{
         myList->last->prox = newNode;
+        newNode->past = myList->last;
         myList->last = newNode;
     }
 
@@ -47,6 +50,7 @@ void dequeue(List* myList){
     else{
         Node* referenceNode = myList->head;
         myList->head = myList->head->prox;
+        myList->head->past = NULL;
         
         free(referenceNode);
         myList->size--;
@@ -74,14 +78,54 @@ int size(List* myList){
     return myList->size;
 }
 
+void swapNodes(List* myList, int index1, int index2){
+    if (index1 == index2) return;
+
+    Node* referenceNode1 = myList->head;
+    Node* referenceNode2 = myList->head;
+
+    while (referenceNode1 && referenceNode1->idade != index1)
+        referenceNode1 = referenceNode1->prox;
+
+    while (referenceNode2 && referenceNode2->idade != index2)
+        referenceNode2 = referenceNode2->prox;
+
+    if (!referenceNode1 || !referenceNode2) return;
+
+    Node* referenceNode1_past = referenceNode1->past;
+    Node* referenceNode1_prox = referenceNode1->prox;
+
+    Node* referenceNode2_past = referenceNode2->past;
+    Node* referenceNode2_prox = referenceNode2->prox;
+
+    referenceNode1->past = referenceNode2_past;
+    referenceNode1->prox = referenceNode2_prox;
+
+    referenceNode2->past = referenceNode1_past;
+    referenceNode2->prox = referenceNode1_prox;
+
+    if (referenceNode1_past) referenceNode1_past->prox = referenceNode2;
+    if (referenceNode1_prox) referenceNode1_prox->past = referenceNode2;
+
+    if (referenceNode2_past) referenceNode2_past->prox = referenceNode1;
+    if (referenceNode2_prox) referenceNode2_prox->past = referenceNode1;
+
+    if (myList->head == referenceNode1)
+        myList->head = referenceNode2;
+
+    else if (myList->head == referenceNode2)
+        myList->head = referenceNode1;
+}
+
 int main(){
-    int n;
+    int n, ex;
     List* myList = queue();
 
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++){
-        enqueue(myList, i+1);
+        scanf("%d", &ex);
+        enqueue(myList, ex);
     }
 
     printf("current size: %d\n", size(myList));
@@ -90,7 +134,6 @@ int main(){
     dequeue(myList);
 
     printf("current size: %d", size(myList));
-
 
     return 0;
 }
